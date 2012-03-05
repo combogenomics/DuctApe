356e377d857cfb36a46340381a2087e72721d420
Marco Galardini <galactus8403@yahoo.it>
2012-03-05 13:14:41 +0100

    Terminal: utilities for command line aplications

---
+++ DuctApe/Terminal.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""
+Terminal
+
+DuctApe Library
+
+Utilities for command line DuctApe programs
+"""
+from DuctApe.Common.TerminalProgress import ProgressBar, TerminalController
+import logging
+import sys
+import time
+
+__author__ = "Marco Galardini"
+
+################################################################################
+# Log setup
+
+logger = logging.getLogger('Terminal')
+
+################################################################################
+# Methods
+
+def RunThread(obj):
+    obj.start()
+    
+    prg = None
+    substatus = 0
+    sub = False
+    while True:
+        time.sleep(0.5)
+        while not obj.msg.empty():
+            msg = obj.msg.get()
+            if msg:
+                if msg.status in obj.getSubStatuses():
+                    if prg is None or substatus != msg.status:
+                        substatus = msg.status
+                        sub = True
+                        logger.info('%s - %s'%(msg.status, msg.msg))
+                        prg = ProgressBar(TerminalController(sys.stdout), msg.msg)
+                if msg.fail:
+                    logger.error('Failure! %s'%msg.msg)
+                    return False
+                elif not msg.substatus and not sub:
+                    logger.info('%s - %s'%(msg.status, msg.msg))
+                    prg = None
+                else:
+                    if msg.maxsubstatus != 0:
+                        prg.update(msg.substatus/float(msg.maxsubstatus),
+                            'Item %d on %d total'%(msg.substatus, msg.maxsubstatus))
+                sub = False
+                
+        if not obj.isAlive():
+            break
+        
+    return True

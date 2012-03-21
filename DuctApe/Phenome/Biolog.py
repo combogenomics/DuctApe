Parameters creation (3): - correct parameters calculation!

DuctApe/Phenome/Biolog.py
@@ -13,6 +13,7 @@ matplotlib.use('Agg')
 from DuctApe.Common.CommonThread import CommonThread
 from DuctApe.Phenome.fitting import fitData
 from DuctApe.Common.utils import smooth, compress
+from scipy.integrate import trapz
 import Queue
 import csv
 import logging
@@ -335,6 +336,12 @@ class BiologRaw(object):
         
         # Parameters
         self.max = None
+        self.min = None
+        self.avg_height = None
+        self.plateau = None
+        self.slope = None
+        self.lag = None
+        self.area = None
         
     def addSignal(self,time,signal):
         self.signals[time] = signal
@@ -412,31 +419,39 @@ class BiologRaw(object):
         if not self.compressed and not noCompress:
             self.compress()
         if not self.smoothed and not noSmooth:
-            self.smooth(window_len=41, window_type='blackman')
+            self.smooth(window_len=len(self.signals)/3, window_type='blackman')
             
         # Let's start with the easy ones!
         self.max = self.getMax()
         
         self.min = self.getMin()
         
-        self.avg_height = np.array( self.signals.values() ).mean()
+        self.height = np.array( self.signals.values() ).mean()
         
         # Let's go with the function fitting
         xdata = np.array( [x for x in sorted(self.signals.keys())] )
         ydata = np.array( [self.signals[x] for x in xdata] )
-        plateau, slope, inflection, y0 = fitData(xdata, ydata)
+        self.plateau, self.slope, self.lag, v, y0 = fitData(xdata, ydata)
         
-        from scipy.integrate import quad
-        from DuctApe.Phenome import fitting
-        # Some esoteric method for area calculation (to be checked!)
-        if plateau:
-            area = quad(fitting.gompertz, xdata.min(), xdata.max(), args=(plateau, slope, inflection, y0))
+        # If any of the values are null generate them by hand
+        if not y0:
+            self.plateau = ydata.max()
+            self.slope = 0
+            self.lag = xdata.max()
+
+        # Trapezoid integration for area calculation
+        self.area = trapz(y = ydata, x = xdata)
+        
+        # Check the plateu and lag parameters
+        if self.lag >= 0:
+            y0 = - (self.lag * self.slope)
         else:
-            area = 0
-
-        print '\t'.join( [self.plate_id, self.well_id] + [str(x) for x in [area, plateau, slope, inflection, y0]] )
-
-class BiologParser(CommonThread):
+            y0 = 0
+        xplateau = (self.plateau - y0) / self.slope
+        if xplateau > xdata.max():
+            self.plateau = ydata.max()
+        
+class BiologParser():
     '''
     Class BiologParser
     Takes a csv output file and returns a series of objects

DuctApe/Phenome/fitting.py
@@ -6,10 +6,23 @@ Phenome library
 
 biolog data fitting functions
 """
+from DuctApe.Common.utils import compress, smooth
 from scipy.optimize.minpack import curve_fit
 import numpy as np
 
-def gompertz(x, A, u, d, y0):
+__author__ = "Marco Galardini"
+
+def logistic(x, A, u, d, v, y0):
+    '''
+    Logistic growth model
+    Taken from: "Modeling of the bacterial growth curve."
+                (Zwietering et al., 1990)
+                PMID: 16348228
+    '''
+    y = (A / (1 + np.exp( ( ((4 * u)/A) * (d - x) ) + 2 ))) + y0
+    return y
+
+def gompertz(x, A, u, d, v, y0):
     '''
     Gompertz growth model
     Taken from: "Modeling of the bacterial growth curve."
@@ -19,14 +32,15 @@ def gompertz(x, A, u, d, y0):
     y = (A * np.exp( -np.exp( (((u * np.e)/A) * (d - x)) + 1 ) ) ) + y0
     return y
 
-def logistic(x, A, u, d, y0):
+def richards(x, A, u, d, v, y0):
     '''
-    Logistic growth model
+    Richards growth model
+    (equivalent to Stannard)
     Taken from: "Modeling of the bacterial growth curve."
                 (Zwietering et al., 1990)
                 PMID: 16348228
     '''
-    y = (A / (1 + np.exp( ( ((4 * u)/A) * (d - x) ) + 2 ))) + y0
+    y = (A * pow(1 + (v + (np.exp(1 + v) * np.exp( (u/A) * (1 + v) * (1 + (1/v)) * (d - x) ) ) ),-(1/v))) + y0
     return y
 
 def getFlex(x, y):
@@ -55,25 +69,48 @@ def getFlex(x, y):
     
     return flex
 
+def rect(x, a, y0):
+    '''
+    yep, that's a rect!
+    '''
+    y = (a * x) + y0
+    return y
+
 def fitData(xdata, ydata):
     '''
     Fits the provided data to the first working function
     (first Gompertz, then Logistic)
 
-    Returns a tuple with plateau, slope, inflection point, y0 and model used
+    Returns a tuple with plateau, slope, lag, y0 and model used
     If no fitting was possible all values are None
 
     Please note that the plateau may be reached outside the final time point
     '''
-    # Initial guesses for the output parameters
-    p0 = [xdata.max(), 4.0, getFlex(xdata, ydata), 0]
+    params = (None, None, None, None, None)
     
-    try:
-        params, pcov = curve_fit(gompertz, xdata, ydata, p0 = p0)
-    except:
+    retries = 3
+    while retries > 0:
+        # Initial guesses for the output parameters
+        p0 = [ydata.max(), 4.0, getFlex(xdata, ydata), 0.1, 0]
+        if retries == 1:
+            p0[2] = 0
         try:
-            params, pcov = curve_fit(logistic, xdata, ydata, p0 = p0)
+            params, pcov = curve_fit(gompertz, xdata, ydata, p0 = p0)
+            break
         except:
-            params = (None, None, None, None)
-        
+            try:
+                params, pcov = curve_fit(logistic, xdata, ydata, p0 = p0)
+                break
+            except:
+                try:
+                    params, pcov = curve_fit(richards, xdata, ydata, p0 = p0)
+                    break
+                except:
+                    retries -= 1
+                    # Compress again the data
+                    xdata = np.array(compress(xdata, span=4))
+                    xdata = np.array(smooth(xdata, window_len = len(xdata)/2, 
+                              window = 'blackman'))
+                    ydata = np.array(compress(ydata, span=4))
+    
     return params
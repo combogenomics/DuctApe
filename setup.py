f13c32f31123a8354ea63f51f9129b0228c2c38e
Marco Galardini <galactus8403@yahoo.it>
2012-08-18 15:42:39 +0200

    Packaging tests

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from ductape import __version__ 
+
+setup(
+    name = 'DuctApe',
+    version = __version__,
+    author = 'Marco Galardini',
+    author_email = 'marco.galardini@unifi.it',
+    packages = ['ductape','ductape.common', 'ductape.genome', 'ductape.kegg',
+                'ductape.phenome', 'ductape.storage', 'ductape.storage.SQLite'], 
+    scripts = ['dape', 'dgenome', 'dphenome'],  
+    #url = 'http://RunnerPyzza', 
+    license = 'LICENSE.txt',
+    description = 'Analyzing and linking genomics and phenomics experiments',
+    #long_description = open('README.txt').read(),
+    install_requires = ['argparse >= 1.1', 'Bio >= 1.5', 'numpy',
+                        'matplotlib >= 1.1', 'multiprocessing',
+                        'SOAPpy', 'scipy', 'sklearn >= 0.10']
+)
+
+
+

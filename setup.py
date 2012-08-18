363dfafbdc131262a086f3ed7e02b106335c11c5
Marco Galardini <galactus8403@yahoo.it>
2012-08-18 17:36:15 +0200

    Setup fixes (2)

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     packages = ['ductape','ductape.common', 'ductape.genome', 'ductape.kegg',
                 'ductape.phenome', 'ductape.storage', 'ductape.storage.SQLite'], 
     scripts = ['dape', 'dgenome', 'dphenome'],  
-    #url = 'http://RunnerPyzza', 
+    url = 'https://combogenomics.github.com/DuctApe', 
     license = 'LICENSE.txt',
     description = 'Analyzing and linking genomics and phenomics experiments',
-    #long_description = open('README.txt').read(),
+    long_description = open('README.txt').read(),
     install_requires = ['argparse >= 1.1', 'biopython >= 1.5', 'numpy',
                         'matplotlib >= 1.1', 'multiprocessing',
                         'SOAPpy', 'scipy', 'scikit-learn >= 0.11']

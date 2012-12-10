e2a497195a6dc4eaf26d73f997d311995d21fa9a
Marco Galardini <galactus8403@gmail.com>
2012-12-10 14:09:39 +0100

    New website

---
+++ js/misc.js
@@ -0,0 +1,18 @@
+!function ($) {
+
+  /*$(function(){
+  			// tooltip
+    $("a[rel=tooltip]")
+      .tooltip()
+    // popover
+     $("a[rel=popover]")
+      .popover({placement:"top"})
+      .click(function(e) {
+        e.preventDefault()
+      })
+   })*/
+   
+   $('.carousel').carousel();
+   
+}(window.jQuery)
+    
